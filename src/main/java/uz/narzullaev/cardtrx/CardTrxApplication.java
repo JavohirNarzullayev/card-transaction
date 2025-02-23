45112cf1a1d9ff0ee77f88675bc4c676086ddf91
Javohir <narzullayevj99@gmail.com>
2025-02-23 13:26:29 +0500

    init commit

---
+++ src/main/java/uz/narzullaev/cardtrx/CardTrxApplication.java
@@ -0,0 +1,62 @@
+package uz.narzullaev.cardtrx;
+
+import io.micrometer.common.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = "uz.narzullaev.cardtrx.*")
+public class CardTrxApplication {
+
+    public static void main(String[] args) {
+        var app = new SpringApplication(CardTrxApplication.class);
+        var env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        var protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        var serverPort = env.getProperty("server.port");
+        var contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        var hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+
+        log.info("""
+                        ----------------------------------------------------------
+                        \tApplication '{}' is running! Access URLs:
+                        \tLocal: \t\t{}://localhost:{}{}
+                        \tExternal: \t{}://{}:{}{}
+                        \tProfile(s): \t{}
+                        \tSwagger: {}://localhost:{}{}
+                        ----------------------------------------------------------
+                        """,
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles(),
+                protocol,
+                serverPort,
+                contextPath+"\b"+env.getProperty("springdoc.swagger-ui.path")
+        );
+    }
+}
---
+++ src/test/java/uz/narzullaev/cardtrx/CardTrxApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.narzullaev.cardtrx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardTrxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
